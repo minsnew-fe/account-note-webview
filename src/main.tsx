5f622fa1b8cd1f1c33408f367ec31c429c16c38b
minsnew-fe <c.minsnew@gmail.com>
2024-11-28 16:59:38 +0900

    feat: global error boundary 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app/App.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App.tsx';
+import './index.css';
+import { ErrorBoundary } from 'react-error-boundary';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary FallbackComponent={() => <div>Error</div>}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>,
 );
